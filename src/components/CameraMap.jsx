786e2f34e318d1c2388e78a44b5da1804a28875e
citeback <citeback@proton.me>
2026-05-04 13:06:28 -0600

    Map: switch to CARTO Voyager tiles, brand cluster markers red

--- src/components/CameraMap.jsx
+++ src/components/CameraMap.jsx
@@ -1002,7 +1002,7 @@ export default function CameraMap() {
       <div style={{ borderRadius: 14, overflow: 'hidden', border: '1px solid var(--border)', height: 520, position: 'relative' }}>
         <MapContainer center={[38.5, -96.5]} zoom={4} style={{ height: '100%', width: '100%' }} zoomControl>
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
             attribution='&copy; <a href="https://carto.com">CARTO</a> | ALPR data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
           />
           <MapEventHandler onBoundsChange={handleBoundsChange} />
